ea2674409ff9d1a0a73af5babc83dab70cb4605d
FranklinCncr <franklin.cncr@gmail.com>
2022-08-14 16:16:01 -0500

    video detail

--- src/containers/MovieDetail.jsx
+++ src/containers/MovieDetail.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getSingleMovie } from '../mocks/movies';
-// import { _videosService } from '../services/videosService';
+//import { _videosService } from '../services/videosService';
 
 const MovieDetail = () => {
   const params = useParams()
@@ -9,15 +9,25 @@ const MovieDetail = () => {
   useEffect(function () {
     const getMovie = async (id) => {
       // const movie = await _videosService.getSingleVideo(id)
-      const movie = getSingleMovie(id)
+      const movie =  getSingleMovie(id)
       setMovie(movie)
     }
     getMovie(params.idMovie)
   }, [params.idMovie])
-  return (
-    <div>
-      {movie.id}
+
+  if(movie.video_files) return (
+    <div className="video-detail-container">
+      
+      <div>
+      <p>Id: {movie.id}</p>
+      <p>Name: {movie.user.name}</p>   
       <button>Añadir al carrito</button>
+      </div>
+
+      <video width="560" height="360" controls>
+        <source src={movie.video_files[2].link} type="video/mp4"/>
+      </video>
+      
     </div>
   );
 };
